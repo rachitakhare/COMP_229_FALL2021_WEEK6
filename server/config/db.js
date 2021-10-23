fbb68a8f5a04e183db7b0184f0f85c1d4d304dda
rachita <rachitakhare15@gmail.com>
2021-10-23 11:10:33 -0400

    Week6- authentication

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://rachita:ZZut1vcHdQI4gVd9@mongodatabaseservers.2kn6s.mongodb.net/book_store?retryWrites=true&w=majority"
+}
